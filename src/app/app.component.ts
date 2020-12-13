82b3d52fa89d4a1b8b6af1523efbcdc14aac30c4
Famous <famousighodaro@gmail.com>
2020-12-14 00:42:11 +0100

    deleted the home page and added form elements to the login page. Also the primary color was changed in the varaible.scss file in the theme folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { MenuController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -9,13 +9,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
-  constructor(
+export class AppComponent implements OnInit{
+  constructor (
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private menuCtrl: MenuController
   ) {
     this.initializeApp();
+  }
+  ngOnInit(): void {
+    
   }
 
   initializeApp() {
@@ -23,5 +27,9 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  onInit(){
+
   }
 }
